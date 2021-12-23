8379819c5b20afcee7739253e5c3965d1c5dfac6
NikolayDPaev <niki.paev@gmail.com>
2021-12-23 14:48:05 +0200

    read write and extract blobs methods added

--- server/commit/commit.go
+++ server/commit/commit.go
@@ -1,7 +1,60 @@
 package commit
 
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/NikolayDPaev/CentralisedVersionControl/server/netIO"
+)
+
 type Commit struct {
-	tree    string
-	message string
-	creator string
+	metadata Metadata
+	tree     string
+}
+
+func ReadCommit(reader io.Reader) (*Commit, error) {
+	id, err := netIO.ReceiveString(reader)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read hash of commit: %w", err)
+	}
+
+	metadata, err := ReadMetadata(reader, id)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read metadata of commit: %w", err)
+	}
+
+	tree, err := netIO.ReceiveString(reader)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read tree string of commit: %w", err)
+	}
+
+	return &Commit{metadata: *metadata, tree: tree}, nil
+}
+
+func (c *Commit) Id() string {
+	return c.metadata.id
+}
+
+func (c *Commit) Write(writer io.Writer) error {
+	if err := c.metadata.Write(writer); err != nil {
+		return fmt.Errorf("cannot write commit: %w", err)
+	}
+
+	if err := netIO.SendString(c.tree, writer); err != nil {
+		return fmt.Errorf("cannot write commit tree: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Commit) ExtractBlobIds() []string { // regex ????
+	lines := strings.Split(c.tree, "\n")
+
+	blobIds := make([]string, len(lines))
+	for i, line := range lines {
+		blobIds[i] = strings.Split(line, " ")[1]
+	}
+
+	return blobIds
 }
