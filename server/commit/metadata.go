e7bab2119c950eee9dd10cea79de505ef4f26995
NikolayDPaev <niki.paev@gmail.com>
2021-12-23 14:47:36 +0200

    write method to metadata added

--- server/commit/metadata.go
+++ server/commit/metadata.go
@@ -2,16 +2,17 @@ package commit
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 )
 
 type Metadata struct {
+	id      string
 	message string
 	creator string
-	hash    string
 }
 
-func NewMetadataFromFile(reader io.Reader, hash string) (*Metadata, error) {
+func ReadMetadata(reader io.Reader, id string) (*Metadata, error) {
 	scanner := bufio.NewScanner(reader)
 	scanner.Scan()
 	message := scanner.Text()
@@ -22,9 +23,24 @@ func NewMetadataFromFile(reader io.Reader, hash string) (*Metadata, error) {
 		return nil, err
 	}
 
-	return &Metadata{message: message, creator: creator, hash: hash}, nil
+	return &Metadata{message: message, creator: creator, id: id}, nil
 }
 
 func (m Metadata) String() string {
-	return m.hash + " \"" + m.message + "\" " + m.creator
+	return m.id + " \"" + m.message + "\" " + m.creator
+}
+
+func (m *Metadata) Write(writer io.Writer) error {
+	bufWriter := bufio.NewWriter(writer)
+
+	_, err := bufWriter.WriteString(m.message + "\n")
+	if err != nil {
+		return fmt.Errorf("cannot write metadata: %w", err)
+	}
+
+	_, err = bufWriter.WriteString(m.creator + "\n")
+	if err != nil {
+		return fmt.Errorf("cannot write metadata: %w", err)
+	}
+	return nil
 }
