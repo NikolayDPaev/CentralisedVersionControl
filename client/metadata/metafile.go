2598c12e004428cff03fff505cfcbc88fa0decea
NikolayDPaev <niki.paev@gmail.com>
2022-02-12 14:39:08 +0200

    metafile renamed

--- client/metadata/metafile.go
+++ client/metadata/metafile.go
@@ -1,22 +1,69 @@
 package metadata
 
 import (
-	"fmt"
-	"os"
+	"bufio"
+	"errors"
 )
 
-func newMetaFile(metafileName string) (*os.File, error) {
-	file, err := os.Create(metafileName)
-	if err != nil {
-		return nil, fmt.Errorf("cannot create metafile: %w", err)
-	}
-	return file, nil
+var ErrMissingMetafile = errors.New("cannot open .cvc file")
+
+// Struct that represents the data of the metafile
+type MetafileData struct {
+	Username       string
+	Address        string
+	FileExceptions map[string]struct{}
 }
 
-func openMetaFile(metafileName string) (*os.File, error) {
-	file, err := os.Open(metafileName)
+// Tries to open the metafile to read it.
+// Returns metafileData struct.
+func ReadMetafileData(metafileName string) (*MetafileData, error) {
+	file, err := openMetaFile(metafileName)
 	if err != nil {
-		return nil, fmt.Errorf("cannot open metafile: %w", err)
+		return nil, ErrMissingMetafile
 	}
-	return file, nil
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var username string
+	var address string
+	if scanner.Scan() && scanner.Err() == nil {
+		username = scanner.Text()
+	}
+
+	if scanner.Scan() && scanner.Err() == nil {
+		address = scanner.Text()
+	}
+
+	exceptions := make(map[string]struct{})
+	for scanner.Scan() && scanner.Err() == nil {
+		exceptions[scanner.Text()] = struct{}{}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return &MetafileData{Username: username, Address: address, FileExceptions: exceptions}, nil
+}
+
+// Saves the metadata struct to a new metafile
+func Save(data *MetafileData, metafileName string) error {
+	file, err := newMetaFile(metafileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	bufWriter := bufio.NewWriter(file)
+	defer bufWriter.Flush()
+	bufWriter.WriteString(data.Username)
+	bufWriter.WriteRune('\n')
+	bufWriter.WriteString(data.Address)
+	bufWriter.WriteRune('\n')
+	for files := range data.FileExceptions {
+		bufWriter.WriteString(files)
+		bufWriter.WriteRune('\n')
+	}
+
+	return nil
 }
--- client/metadata/metafileio.go
+++ client/metadata/metafileio.go
@@ -1,69 +1,22 @@
 package metadata
 
 import (
-	"bufio"
-	"errors"
+	"fmt"
+	"os"
 )
 
-var ErrMissingMetafile = errors.New("cannot open .cvc file")
-
-// Struct that represents the data of the metafile
-type MetafileData struct {
-	Username       string
-	Address        string
-	FileExceptions map[string]struct{}
+func newMetaFile(metafileName string) (*os.File, error) {
+	file, err := os.Create(metafileName)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create metafile: %w", err)
+	}
+	return file, nil
 }
 
-// Tries to open the metafile to read it.
-// Returns metafileData struct.
-func ReadMetafileData(metafileName string) (*MetafileData, error) {
-	file, err := openMetaFile(metafileName)
+func openMetaFile(metafileName string) (*os.File, error) {
+	file, err := os.Open(metafileName)
 	if err != nil {
-		return nil, ErrMissingMetafile
+		return nil, fmt.Errorf("cannot open metafile: %w", err)
 	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	var username string
-	var address string
-	if scanner.Scan() && scanner.Err() == nil {
-		username = scanner.Text()
-	}
-
-	if scanner.Scan() && scanner.Err() == nil {
-		address = scanner.Text()
-	}
-
-	exceptions := make(map[string]struct{})
-	for scanner.Scan() && scanner.Err() == nil {
-		exceptions[scanner.Text()] = struct{}{}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	return &MetafileData{Username: username, Address: address, FileExceptions: exceptions}, nil
-}
-
-// Saves the metadata struct to a new metafile
-func Save(data *MetafileData, metafileName string) error {
-	file, err := newMetaFile(metafileName)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	bufWriter := bufio.NewWriter(file)
-	defer bufWriter.Flush()
-	bufWriter.WriteString(data.Username)
-	bufWriter.WriteRune('\n')
-	bufWriter.WriteString(data.Address)
-	bufWriter.WriteRune('\n')
-	for files := range data.FileExceptions {
-		bufWriter.WriteString(files)
-		bufWriter.WriteRune('\n')
-	}
-
-	return nil
+	return file, nil
 }
